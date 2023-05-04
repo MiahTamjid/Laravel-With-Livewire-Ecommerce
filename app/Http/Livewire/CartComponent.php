2cf1e3a12dabf831442fea7edda636f01d598828
Miah Mohammad Tamjid <69211023+MiahTamjid@users.noreply.github.com>
2023-05-04 11:32:10 +0600

    Adding Product To the Wishlist

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -14,26 +14,26 @@ class CartComponent extends Component
 
     public function increaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
 
     }
     public function decreaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
-        Cart::update($rowId,$qty);
+        Cart::instance('cart')->update($rowId,$qty);
 
     }
     public function deleteProduct($rowId)
     {
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
         session()->flash('success_message','Item has been Removed!');
     }
     public function deleteProductAll()
     {
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
         session()->flash('success_message','All Item has been Removed!');
     }
 
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -153,8 +153,10 @@
 							<div class="wrap-icon-section wishlist">
 								<a href="#" class="link-direction">
 									<i class="fa fa-heart" aria-hidden="true"></i>
-									<div class="left-info">
-										<span class="index">0 item</span>
+									<div class="left-info">	
+										@if(Cart::instance('wishlist')->count() > 0)									
+											<span class="index">{{Cart::instance('wishlist')->count()}} item</span>		
+										@endif											
 										<span class="title">Wishlist</span>
 									</div>
 								</a>
@@ -163,8 +165,7 @@
 								<a href="#" class="link-direction">
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
-										@if (Cart::count() > 0)
-											
+										@if (Cart::instance('cart')->count() > 0)
 										
 										<span class="index">{{ Cart::count() }} items</span>
 										<span class="title">CART</span>
